753c8c3a541a9429f2d3fe1b76af7d75e79c07e3
Eric Lewis <eric.andrew.lewis@gmail.com>
2018-03-28 07:18:37 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const iframe = document.querySelector(".document-display-iframe");
 const editor = CodeMirror(document.querySelector(".codemirror-root"), {
   value: "<road></road>",
-  mode: "htmlmixed",
+  mode: "text/html",
   theme: "one-dark",
   tabSize: 2,
   showCursorWhenSelecting: true,
   undoDepth: 1000,
-  autofocus: true
+  autofocus: true,
+  autoCloseTags: true
 });
 
 const updateIframeDoc = html => {
@@ -15,9 +16,22 @@ const updateIframeDoc = html => {
 
 updateIframeDoc(editor.doc.getValue());
 
-iframe.contentWindow.document.head.innerHTML = `<style>
-body { background-color: #00B516; }
-road { display: block; background-image: url(road.svg); height: 100px; background-repeat: repeat; margin-bottom: 1rem; }
+iframe.contentWindow.document.head.innerHTML = `
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.css" />
+<style>
+body {
+  background-color: #00B516;
+}
+road {
+  display: block;
+  height: 100px;
+  background-image: url(road.svg);
+  background-repeat: repeat;
+  margin-bottom: 1rem;
+}
+road:nth-child(1) {
+  margin-top: 1rem;
+}
 </style>`;
 
 editor.on("change", (instance, changeObj) => {
